4e2dae80409daad0eaffa539eb8b795bb0300200
Jose <Jose@bodhilinux.com>
2011-09-14 09:15:16 +0530

    incremental

--- app/config/core.php
+++ app/config/core.php
@@ -246,9 +246,9 @@
 *
 **/
 
-Configure::write('Appcenter.build','Incremental-BossyDame');
+Configure::write('Appcenter.build','Build: Incremental-BossyDame');
 Configure::write('Appcenter.year','2011');
-Configure::write('Appcenter.copyright','Bodhilinux Web Team');
+Configure::write('Appcenter.copyright','(c) Bodhilinux Web Team');
 /**
  * If you are on PHP 5.3 uncomment this line and correct your server timezone
  * to fix the date & time related errors.

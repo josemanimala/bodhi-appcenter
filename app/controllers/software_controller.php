bce0f006c34d8fc37457c5c55cd2b991f50b660d
Jose <Jose@bodhilinux.com>
2011-10-07 22:41:47 +0530

    minor feed bug

--- app/controllers/software_controller.php
+++ app/controllers/software_controller.php
@@ -80,7 +80,7 @@ function searchPost()
 }
 function generatefeed(){
 
-	$software = $this->Software->find('all',array('order'=>array('Software.entry_date DESC')));
+	$software = $this->Software->find('all',array('order'=>array('Software.entry_date DESC'),'limit' => 20));
 	if(isset($this->params['requested'])) {
                          return $software;
                  }

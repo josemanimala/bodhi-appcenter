ff309abe948f5b5ea4cfa15aa613e32409d1ae4e
Jose <Jose@bodhilinux.com>
2011-10-07 22:12:05 +0530

    minor feed bug

--- app/config/routes.php
+++ app/config/routes.php
@@ -27,7 +27,7 @@
  * to use (in this case, /app/views/pages/home.ctp)...
  */
 	Router::connect('/', array('controller' => 'pages', 'action' => 'display', 'home'));
-	Router::connect('/software/feed', array('controller' => 'software', 'action' => 'display', 'generatefeed'));
+	Router::connect('/software/feed', array('controller' => 'software', 'action' => 'generatefeed'));
 /**
  * ...and connect the rest of 'Pages' controller's urls.
  */

9f0b86cf71cd47642b7e93bed2e580f109d878a5
Jose Mathew Manimala <josemanimala@gmail.com>
2011-11-23 00:11:45 +0530

    order feature

--- app/controllers/pages_controller.php
+++ app/controllers/pages_controller.php
@@ -75,7 +75,7 @@ class PagesController extends AppController {
 			$this->set("w00t".$i,$tmp1);
 			$i++;
 		}
-		print_r($order);
+		print_r($itemCat['Catorder']['catName']);
 		$temp = array('test');
 		$temp1 = array('test');
 		foreach($data as $item)
